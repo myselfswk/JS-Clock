3ccc5fb37b682d9ecb380eb149ad76aaa0489ab1
Muhammad Waleed Khan <41314353+myselfswk@users.noreply.github.com>
2020-11-11 21:52:28 +0500

    Add files via upload
    upload source code and image files

---
+++ script.js
@@ -0,0 +1,49 @@
+//Analog Clock
+
+const deg = 6;
+const hr = document.getElementById('hr');
+const mn = document.getElementById('mn');
+const sc = document.getElementById('sc');
+
+setInterval(()=>{
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+    
+    hr.style.transform = `rotateZ(${(hh) + (mm / 12)}deg)`
+    mn.style.transform = `rotateZ(${mm}deg)`
+    sc.style.transform = `rotateZ(${ss}deg)`
+});
+
+// Digital Clock
+
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
